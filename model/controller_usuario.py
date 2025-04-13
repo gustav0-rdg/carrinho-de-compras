37798b08bbffa48d2b2e5d0e8611173753b8f660
Gustavo <gustavolourenco821@gmail.com>
2025-04-13 10:10:38 -0300

    funcao para logar o usuario

--- model/controller_usuario.py
+++ model/controller_usuario.py
@@ -1,6 +1,7 @@
 import mysql.connector
 from hashlib import sha256
 from data.conexao import Conexao
+from flask import session
 
 class Usuario:
     def cadastrar(usuario, nome, senha):
@@ -12,7 +13,7 @@ class Usuario:
             sql = "INSERT INTO tbusuarios(usuario, nome, senha) VALUES(%s, %s, %s)"
             valores = (usuario, nome, senha)
             cursor.execute(sql, valores)
-            cursor.commit()
+            conexao.commit()
 
         except Exception as e:
             print(f'O erro é {e}')
@@ -20,3 +21,24 @@ class Usuario:
             #  Se a variavel conexao existir, ela será fechada
             if 'conexao' in locals():
                 conexao.close()
+
+    def logar(usuario, senha):  
+        senha = sha256(str(senha).encode()).hexdigest()
+        try:
+            conexao = Conexao.criar_conexao()
+            cursor = conexao.cursor(dictionary=True)
+            sql = "SELECT * FROM tbusuarios WHERE usuario = %s AND BINARY senha = %s"
+            valores = (usuario, senha)
+            cursor.execute(sql, valores)
+            resultado = cursor.fetchone()
+
+            if resultado: 
+                session['usuario'] = resultado['usuario']
+                session['nome'] = resultado['nome']
+                return True
+            else:
+                return False
+        except Exception as e:
+            print(f'Erro: {e}')
+        finally:
+            conexao.close()
